d6006d3228b4ae8463e9dbad75a41b73c27ae5d8
Shrey Bhardwaj <shrey.bhardwaj@juspay.in>
2023-04-02 00:25:20 +0530

    added nvimtree, blame and gitsigns

--- lua/shrey/mappings.lua
+++ lua/shrey/mappings.lua
@@ -1,7 +1,5 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-
 vim.keymap.set("n", ";", ":")
 
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
--- lua/shrey/packer.lua
+++ lua/shrey/packer.lua
@@ -52,4 +52,12 @@ return require('packer').startup(function(use)
 	  }
   }
 
+  use ( 'f-person/git-blame.nvim' )
+
+  use ( 'nvim-tree/nvim-tree.lua' )
+
+  use ( 'nvim-tree/nvim-web-devicons' )
+
+  use ( 'lewis6991/gitsigns.nvim' )
+
 end)
