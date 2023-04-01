84e38ad41405a70a237cb26dece7e0715b9851af
Shrey Bhardwaj <shrey.bhardwaj@juspay.in>
2023-04-01 23:40:22 +0530

    mappings added

--- lua/shrey/mappings.lua
+++ lua/shrey/mappings.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("n", ";", ":")
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
